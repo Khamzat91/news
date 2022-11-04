b8c62d619514334a278a2611958cf7d378cd06d3
Khamzat91 <khamzat91@mail.ru>
2022-11-04 23:16:43 +0300

    added container

---
+++ src/components/about/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { pupkin } from "../../images/pupkin.png";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about__inner">
+        <div className="about__inner-title">Vasya Pupkin</div>
+        <div className="about__inner-subtitle">Блог фронтенд-разработчика</div>
+        <img className="about__inner-image" src={pupkin} alt="" />
+        <div className="about__inner-description">
+          <div className="about__inner-description__title">Обо мне</div>
+          <div className="about__inner-description__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            scelerisque diam arcu risus. Imperdiet dolor, porttitor pellentesque
+            fringilla aliquet sit. Turpis arcu vitae quis nunc suscipit. Mattis
+            scelerisque leo curabitur faucibus. Nec, sed porta ac enim. Mattis
+            quam accumsan ipsum commodo sed purus mi. Platea sit lectus neque,
+            nulla sapien vitae nulla. Nisl viverra viverra quis mattis tincidunt
+            laoreet amet, laoreet proin. Duis mi, aliquam tincidunt amet
+            phasellus malesuada non nisi.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
