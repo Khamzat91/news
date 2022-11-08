7ec8734d2afc4af330b6bd5d89c41436d4e69cdc
Khamzat91 <khamzat91@mail.ru>
2022-11-08 22:44:05 +0300

    add component Menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from './components/about/About';
 import Article from './components/article/Article';
+import Menu from './components/menu/Menu';
 
 
 function App() {
   return (
     <div className="app" style={{display: "flex"}}>
     <About/>
+    <Menu/>
     </div>
   );
 }
