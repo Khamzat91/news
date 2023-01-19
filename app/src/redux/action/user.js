4479b43cfaf1a5f26c61f6c6267046d0dd45d76f
Khamzat91 <khamzat91@mail.ru>
2023-01-19 22:45:12 +0300

    создал запрос на получения articles, comments

--- app/src/redux/action/user.js
+++ app/src/redux/action/user.js
@@ -1,21 +1,28 @@
-import { IS_AUTH } from "../types"
+import { IS_AUTH } from "../types";
 import axios from "axios";
 
-
 export const setIsAuth = (isAuth) => ({
-type: IS_AUTH,
-payload: isAuth
-})
+  type: IS_AUTH,
+  payload: isAuth,
+});
 
 export const setUserData = (data, url, navigate) => async (dispatch) => {
-   const response = await axios.post(`http://localhost:5656/auth/${url}`, data)
-   localStorage.setItem('user', JSON.stringify(response.data))
-   dispatch(setIsAuth(true))
-   navigate('/profile')
-}
+  const response = await axios.post(`http://localhost:5656/auth/${url}`, data);
+  localStorage.setItem("user", JSON.stringify(response.data));
+  dispatch(setIsAuth(true));
+  navigate("/profile");
+};
 
 export const logout = () => async (dispatch) => {
-   localStorage.removeItem('user')
-   dispatch(setIsAuth(false))
-}
+  localStorage.removeItem("user");
+  dispatch(setIsAuth(false));
+};
 
+export const getUserData = () => async (dispatch) => {
+  const token = JSON.parse(localStorage.getItem("user")).token;
+  const userId = JSON.parse(localStorage.getItem("user"))._id;
+ const response = await axios.get(`http://localhost:5656/auth/${userId}`, {
+    headers: { Authorization: token },
+  });
+  dispatch(response)
+};
