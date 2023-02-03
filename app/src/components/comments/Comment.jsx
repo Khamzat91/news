564d2ed6f38278a9e6b09bcf0fcba85e50db457d
Khamzat91 <khamzat91@mail.ru>
2023-02-03 22:08:29 +0300

    init

--- app/src/components/comments/Comment.jsx
+++ app/src/components/comments/Comment.jsx
@@ -1,57 +1,35 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { createComment } from "../../redux/action/comments";
-import './index.scss';
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./index.scss";
 
 const option = {
   year: "numeric",
   month: "long",
   day: "numeric",
   hour: "numeric",
-  minute: "numeric"
-}
+  minute: "numeric",
+};
 
-const Comment = () => {
-  const [text, setText] = React.useState('');
-  const dispatch = useDispatch()
-  const {id} = useParams()
-  const changeComment = (e) => {
-    setText(e.target.value)
-  }
-
-  const onSubmitComment = ({text}) => {
-    dispatch(createComment({text: text, postId: id}))
-    setText('')
-  }
-
-// const date = new Date(comments.createdAt)
+const Comment = ({ text, createdAt }) => {
+  const date = new Date(createdAt);
 
   return (
-    <div className="comment">
-      <div className="comment__inner">
-        <div className="comment__inner-title">Комментарии (3)</div>
-        <div className="comment__inner-post">
-          <div className="comment__inner-post__info">
-            <div className="comment__inner-post__name"></div>
-            <div className="comment__inner-post__date">
-              {/* {date.toLocaleDateString("ru-RU", option)} */}
-            </div>
-          </div>
-          <div className="comment__inner-post__text">
-            {text}
-          </div>
+    <div className="comment__inner-post">
+      <div className="comment__inner-post__info">
+        <div className="comment__inner-post__name"></div>
+        <div className="comment__inner-post__date">
+          {date?.toLocaleDateString("ru-RU", option)}
         </div>
-        <div className="comment__inner-form">
-          <div className="comment__inner-form__title">
-            Добавить комментарий
-          </div>
-          <label className="comment__inner-form__label">
-            <textarea value={text} onChange={changeComment} type="text" />
-            <button className="comment__inner-form__btn" onClick={onSubmitComment}>Отправить</button>
-          </label>
-        </div>
+        <Stack direction="row" alignItems="center" spacing={1}>
+        <IconButton aria-label="delete" size="small">
+          <DeleteIcon fontSize="inherit" />
+        </IconButton>
+      </Stack>
       </div>
+      <div className="comment__inner-post__text">{text}</div>
+     
     </div>
   );
 };
--- app/src/components/comments/Comments.jsx
+++ app/src/components/comments/Comments.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Comment from './Comment';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { createComment } from "../../redux/action/comments";
+import Comment from "./Comment";
 
 const Comments = () => {
-    const comments = useSelector(state => state.comments)
-    console.log(comments);
-    return (
-        <div className="comment">
-          {comments.map((comment) => <Comment key={comment._id}
-          text={comment.text}
-          id={comment._id}
-          createdAt={comment.createdAt}
-          />)}  
+  const [text, setText] = React.useState("");
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const comments = useSelector((state) => state.comments.comments);
+  console.log(comments);
+
+  const changeComment = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmitComment = () => {
+    dispatch(createComment({ text: text, postId: id }));
+    setText("");
+  };
+
+  return (
+    <div className="comments">
+      <div className="comment">
+        <div className="comment__inner">
+          <div className="comment__inner-title">Комментарии ({comments.length})</div>
+          {comments.map((comment) => (
+            <Comment
+              key={comment._id}
+              text={comment.text}
+              id={comment._id}
+              createdAt={comment.createdAt}
+            />
+          ))}
+          <div className="comment__inner-form">
+            <div className="comment__inner-form__title">
+              Добавить комментарий
+            </div>
+            <label className="comment__inner-form__label">
+              <textarea value={text} onChange={changeComment} type="text" />
+              <button
+                className="comment__inner-form__btn"
+                onClick={onSubmitComment}
+              >
+                Отправить
+              </button>
+            </label>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
