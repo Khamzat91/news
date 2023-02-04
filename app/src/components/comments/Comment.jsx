1f4d1982f5102ca23dec3acfa311994c7ee924dc
Khamzat91 <khamzat91@mail.ru>
2023-02-04 23:30:53 +0300

    init

--- app/src/components/comments/Comment.jsx
+++ app/src/components/comments/Comment.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from '@mui/icons-material/Edit';
 import "./index.scss";
 
 const option = {
@@ -17,19 +18,25 @@ const Comment = ({ text, createdAt }) => {
 
   return (
     <div className="comment__inner-post">
+      <div className="comment__inner-post__text">{text}</div>
       <div className="comment__inner-post__info">
         <div className="comment__inner-post__name"></div>
         <div className="comment__inner-post__date">
           {date?.toLocaleDateString("ru-RU", option)}
         </div>
-        <Stack direction="row" alignItems="center" spacing={1}>
-        <IconButton aria-label="delete" size="small">
-          <DeleteIcon fontSize="inherit" />
-        </IconButton>
-      </Stack>
+        <div className="comment__inner-post__info-icon">
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <IconButton aria-label="delete" size="small">
+              <DeleteIcon fontSize="inherit" />
+            </IconButton>
+            <IconButton aria-label="delete" size="small">
+              <EditIcon fontSize="inherit" />
+            </IconButton>
+
+          </Stack>
+        </div>
       </div>
-      <div className="comment__inner-post__text">{text}</div>
-     
+      
     </div>
   );
 };
